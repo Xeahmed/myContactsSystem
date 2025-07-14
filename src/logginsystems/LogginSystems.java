25bd339c6e8522419dc4c05602ccef274d101edd
XEAhmed2 <a.xp2121@gmail.com>
2025-07-14 17:22:02 +0300

    Initial commit - Upload myContactsSystem project

---
+++ src/logginsystems/LogginSystems.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logginsystems;
+
+/**
+ *
+ * @author HP
+ */
+public class LogginSystems {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+         Loggin.frmLoggin f = new Loggin.frmLoggin();
+          f.setLocationRelativeTo(null);
+           f.setVisible(true);
+           
+    
+    }
+    
+}
